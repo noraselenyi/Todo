12ff335744ed569d232b07459a567702ce282889
eselnor <noraselenyi@gmail.com>
2022-01-14 15:59:13 +0100

    Updates

---
+++ precache-manifest.758ebb6c193fe6c85d3e034b57ab4f9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe8b2d33c6e70ec0b02fb5babada6a1",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "7da73fce4e2f47284c49",
+    "url": "/Todo/static/css/main.21abdc70.chunk.css"
+  },
+  {
+    "revision": "a1f10869e653cc7a518b",
+    "url": "/Todo/static/js/2.7c3ab427.chunk.js"
+  },
+  {
+    "revision": "7da73fce4e2f47284c49",
+    "url": "/Todo/static/js/main.addf996a.chunk.js"
+  },
+  {
+    "revision": "6e1ac461000570272709",
+    "url": "/Todo/static/js/runtime~main.9145d80b.js"
+  }
+]);
